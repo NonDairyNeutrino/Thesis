def0dbfc9e0f82301d5c569ccc7fa832974dde60
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-03-24 12:00:28 -0700

    begin main program

---
+++ ParticleProduction.jl/src/ParticleProduction.jl
@@ -0,0 +1,13 @@
+module ParticleProduction
+# Outline
+# Get user defined scaling function and other computational parameters
+# Create the vector of conformal time elements based on the scaling function
+# Define phase coefficients chi[eta] at each conformal time
+# Create matrix F of finite-difference coefficients
+# Create solution vector of zeros except for the first two elements are defined by initial conditions value and derivative
+# Solve the matrix equation F * chi[eta] == 0
+
+include("initialize.jl")
+include("conformal.jl")
+
+end
