ef006cf42709743ef9d7891429c464c95c598ae8
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-03-25 14:04:42 -0700

    move files

--- two_dimensional/julia/ConformalTime.jl
+++
@@ -1,9 +0,0 @@
-# define scaling and time here for testing purposes
-scaling(timeScale, labTime) = exp(- labTime / timeScale) # de Sitter expansion
-labTimeVector               = 0 : 0.01 : 10
-
-timeScale                           = 1
-conformalTimeVector                 = sqrt.(scaling.(timeScale, labTimeVector)) |> cumsum
-conformalTimeScaledVector           = scaling.(timeScale, conformalTimeVector)
-conformalTimeScaledDerivativeVector = (conformalTimeScaledVector[2:end] - conformalTimeScaledVector[1:end-1]) ./ (conformalTimeVector[2:end] - conformalTimeVector[1:end-1]) # first order accurate backward difference
-hubbleVector                        = conformalTimeScaledDerivativeVector ./ conformalTimeScaledVector[2:end]
