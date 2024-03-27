c4182e65f20685a12649627b177adfdcf6691623
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-03-27 14:31:09 -0700

    add QuadGK

--- two_dimensional/julia/ParticleProduction.jl/src/conformal.jl
+++ two_dimensional/julia/ParticleProduction.jl/src/conformal.jl
@@ -1,4 +1,5 @@
 # define conformal time
+using QuadGK
 
 """
     conformalTime(scaling :: Function, step :: Float64, finalTime :: Float64) :: Vector{T} where T <: Number
@@ -6,6 +7,7 @@
 Generate the sequence of conformal time values.
 """
 function conformalTime(scaling :: Function, step :: Float64, finalTime :: Float64) :: Vector
+    # TODO: implement integration using quadgk(t -> sqrt(scaling(t)), 0, tn) for tn in labTimeVector
     labTimeVector = 0 : step : finalTime
     conformalTimeVector                 = sqrt.(scaling.(labTimeVector)) |> cumsum
     conformalTimeScaledVector           = scaling.(conformalTimeVector)
